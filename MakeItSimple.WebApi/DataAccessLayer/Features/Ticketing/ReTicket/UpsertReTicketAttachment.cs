3274488c5f4057a076dd25b62802cb9e67d00bc5
philip crasco <philiplorenzcrasco@gmail.com>
2024-02-15 15:40:08 +0800

    fix: database

--- MakeItSimple.WebApi/DataAccessLayer/Features/Ticketing/ReTicket/UpsertReTicketAttachment.cs
+++ MakeItSimple.WebApi/DataAccessLayer/Features/Ticketing/ReTicket/UpsertReTicketAttachment.cs
@@ -1,6 +1,55 @@
-﻿namespace MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.ReTicket
+﻿using CloudinaryDotNet;
+using MakeItSimple.WebApi.Common;
+using MakeItSimple.WebApi.Common.Cloudinary;
+using MakeItSimple.WebApi.DataAccessLayer.Data;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.ReTicket
 {
     public class UpsertReTicketAttachment
     {
+        public class UpsertReTicketAttachmentCommand : IRequest<Result>
+        {
+            public int? RequestGeneratorId { get; set; }
+
+            public Guid? Added_By { get; set; }
+            public Guid? Modified_By { get; set; }
+
+            public List<ReTicketFile> ReTicketFiles { get; set; }
+
+            public class ReTicketFile
+            {
+                public int? TicketAttachmentId { get; set; }
+                public IFormFile Attachment { get; set; }
+            }
+
+            
+
+        }
+
+        public class Handler : IRequestHandler<UpsertReTicketAttachmentCommand, Result>
+        {
+            private readonly MisDbContext _context;
+            private readonly Cloudinary _cloudinary;
+
+            public Handler(MisDbContext context, IOptions<CloudinaryOption> options)
+            {
+                _context = context;
+                var account = new Account(
+                   options.Value.Cloudname,
+                   options.Value.ApiKey,
+                   options.Value.ApiSecret);
+                _cloudinary = new Cloudinary(account);
+            }
+
+            public async Task<Result> Handle(UpsertReTicketAttachmentCommand request, CancellationToken cancellationToken)
+            {
+
+
+                await _context.SaveChangesAsync(cancellationToken);
+                return Result.Success();
+            }
+        }
     }
 }
--- MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
+++ MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e197e99e77b38c773efad150e23b2e0c06b8a15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

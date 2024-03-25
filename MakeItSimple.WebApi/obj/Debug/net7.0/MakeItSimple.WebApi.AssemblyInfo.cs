c7372a62b950f924b03259deb74d1246a3d79b33
philip crasco <philiplorenzcrasco@gmail.com>
2024-03-25 22:27:47 +0800

    fix : Sub unit and Approver

--- MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
+++ MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f16dea0f5e4442eb05d5d81fd94a270679cca87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

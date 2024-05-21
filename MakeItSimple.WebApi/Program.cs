2ad3fb6fb7fa4c593faded8906028fea8ef2d0f3
philip crasco <philiplorenzcrasco@gmail.com>
2024-05-21 16:17:54 +0800

    Changes

--- MakeItSimple.WebApi/Program.cs
+++ MakeItSimple.WebApi/Program.cs
@@ -22,7 +22,7 @@ var config = builder.Configuration;
 // Add services to the container.
 
 
-var connectionString = builder.Configuration.GetConnectionString("Testing");
+var connectionString = builder.Configuration.GetConnectionString("Test");
 
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
 builder.Services.AddDbContext<MisDbContext>(x =>

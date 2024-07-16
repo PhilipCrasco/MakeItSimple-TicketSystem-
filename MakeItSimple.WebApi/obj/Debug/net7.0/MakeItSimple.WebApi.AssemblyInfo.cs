ec56c0b3b5f96485bed3e990c79c83c176f22c74
philip crasco <philiplorenzcrasco@gmail.com>
2024-07-16 11:45:03 +0800

    update this fucking bitch

--- MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
+++ MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bbeeb592ef7f3a4d3b8435c12cbfbc077e77c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379a706b30e0d17996e977a3a71b45bf5f6e0d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

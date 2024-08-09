e8624629c3b250601b810b23a5a2f6804e5c8936
philip crasco <philiplorenzcrasco@gmail.com>
2024-08-09 11:15:32 +0800

    Update test in user

--- MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
+++ MakeItSimple.WebApi/obj/Debug/net7.0/MakeItSimple.WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea3c27d1f5c2e89da453ae1581dfd0c55b36a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ceeb04b723b32b8293b361e4534aa5637d8896")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakeItSimple.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

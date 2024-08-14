745546b0208d741e811fd7894e94fcbbbc9c28be
philip crasco <philiplorenzcrasco@gmail.com>
2024-08-14 14:28:33 +0800

    Update Notification signal r

--- MakeItSimple.WebApi/Controllers/Ticketing/TicketingNotificationController.cs
+++ MakeItSimple.WebApi/Controllers/Ticketing/TicketingNotificationController.cs
@@ -1,19 +1,13 @@
-﻿
-using MakeItSimple.WebApi.Common.SignalR;
+﻿using MakeItSimple.WebApi.Common.SignalR;
 using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
-using System.Security.Claims;
 using static MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.TicketingNotification.ClosingTicketNotification;
 using static MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.TicketingNotification.CommentNotification;
 using static MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.TicketingNotification.OpenTicketNotification;
 using static MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.TicketingNotification.TicketConcernNotification;
 using static MakeItSimple.WebApi.DataAccessLayer.Features.Ticketing.TicketingNotification.TransferTicketNotification;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-
+using System.Security.Claims;
 
 namespace MakeItSimple.WebApi.Controllers.Ticketing
 {
@@ -23,250 +17,86 @@ namespace MakeItSimple.WebApi.Controllers.Ticketing
     {
         private readonly IMediator _mediator;
         private readonly TimerControl _timerControl;
-        private readonly IHubContext<NotificationHub> _client;
+        private readonly IHubContext<NotificationHub> _hubContext;
 
-        public TicketingNotificationController(IMediator mediator, TimerControl timerControl, IHubContext<NotificationHub> client)
+        public TicketingNotificationController(IMediator mediator, IHubContext<NotificationHub> hubContext, TimerControl timerControl)
         {
             _mediator = mediator;
-            _client = client;
+            _hubContext = hubContext;
             _timerControl = timerControl;
+        }
+
+        private async Task<IActionResult> HandleNotification<T>(T command, string notificationType)
+        {
+            try
+            {
+                if (User.Identity is ClaimsIdentity identity)
+                {
+                    var userRole = identity.FindFirst(ClaimTypes.Role);
+                    if (userRole != null)
+                    {
+                        dynamic cmd = command;
+                        cmd.Role = userRole.Value;
+                    }
+
+                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
+                    {
+                        dynamic cmd = command;
+                        cmd.UserId = userId;
+                    }
+                }
+
+                var results = await _mediator.Send(command);
+
+                if (_timerControl != null && !_timerControl.IsTimerStarted)
+                {
+                    _timerControl.ScheduleTimer(async (scopeFactory) =>
+                    {
+                        using var scope = scopeFactory.CreateScope();
+                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                        var requestData = await mediator.Send(command);
+                        await _hubContext.Clients.All.SendAsync(notificationType, requestData);
+                    }, 2000);
+                }
+
+                await _hubContext.Clients.All.SendAsync("ReceiveNotification", "New data has been received or sent.");
+
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
         }
 
         [HttpGet("ticket-request")]
         public async Task<IActionResult> RequestTicketNotification([FromQuery] RequestTicketNotificationResultQuery command)
         {
-            try
-            {
-                if (User.Identity is ClaimsIdentity identity)
-                {
-                    var userRole = identity.FindFirst(ClaimTypes.Role);
-                    if (userRole != null)
-                    {
-                        command.Role = userRole.Value;
-                    }
-
-                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
-                    {
-                        command.UserId = userId;
-
-                    }
-                }
-                var results = await _mediator.Send(command);
-                if (results.IsFailure)
-                {
-                    return BadRequest(results);
-                }
-
-                var timerControl = _timerControl;
-                var clientsAll = _client.Clients.All;
-
-                if (timerControl != null && !timerControl.IsTimerStarted && clientsAll != null)
-                {
-                    timerControl.ScheduleTimer(async (scopeFactory) =>
-                    {
-                        using var scope = scopeFactory.CreateScope();
-                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                        var requestData = await mediator.Send(command);
-                        await clientsAll.SendAsync("TicketRequestData", requestData);
-
-                    }, 2000);
-                }
-
-                await _client.Clients.All.SendAsync("ReceiveNotification", "New data has been received or sent.");
-
-                return Ok(results);
-            }
-            catch (Exception ex)
-            {
-                return Conflict(ex.Message);
-            }
+            return await HandleNotification(command, "TicketRequestData");
         }
 
         [HttpGet("transfer-ticket")]
         public async Task<IActionResult> TransferTicketNotification([FromQuery] TransferTicketNotificationResultQuery command)
         {
-            try
-            {
-                if (User.Identity is ClaimsIdentity identity)
-                {
-                    var userRole = identity.FindFirst(ClaimTypes.Role);
-                    if (userRole != null)
-                    {
-                        command.Role = userRole.Value;
-                    }
-
-                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
-                    {
-                        //query.Users = userId;
-                        command.UserId = userId;
-
-                    }
-                }
-
-                var results = await _mediator.Send(command);
-                if (results.IsFailure)
-                {
-                    return BadRequest(results);
-                }
-
-                var timerControl = _timerControl;
-                var clientsAll = _client.Clients.All;
-
-                if (timerControl != null && !timerControl.IsTimerStarted && clientsAll != null)
-                {
-                    timerControl.ScheduleTimer(async (scopeFactory) =>
-                    {
-                        using var scope = scopeFactory.CreateScope();
-                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                        var requestData = await mediator.Send(command);
-                        await clientsAll.SendAsync("TransferData", requestData);
-                    }, 2000);
-                }
-
-                await _client.Clients.All.SendAsync("ReceiveNotification", "New data has been received or sent.");
-
-                return Ok(results);
-            }
-            catch (Exception ex)              
-            {
-                return Conflict(ex.Message);
-            }
+            return await HandleNotification(command, "TransferData");
         }
-
 
         [HttpGet("closing-ticket")]
         public async Task<IActionResult> ClosingTicketNotification([FromQuery] ClosingTicketNotificationResultQuery command)
         {
-            try
-            {
-                if (User.Identity is ClaimsIdentity identity)
-                {
-                    var userRole = identity.FindFirst(ClaimTypes.Role);
-                    if (userRole != null)
-                    {
-                        command.Role = userRole.Value;
-                    }
-
-                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
-                    {
-                        //query.Users = userId;
-                        command.UserId = userId;
-
-                    }
-                }
-                var results = await _mediator.Send(command);
-                if (results.IsFailure)
-                {
-                    return BadRequest(results);
-                }
-
-                var timerControl = _timerControl;
-                var clientsAll = _client.Clients.All;
-
-                if (timerControl != null && !timerControl.IsTimerStarted && clientsAll != null)
-                {
-                    timerControl.ScheduleTimer(async (scopeFactory) =>
-                    {
-                        using var scope = scopeFactory.CreateScope();
-                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                        var requestData = await mediator.Send(command);
-                        await clientsAll.SendAsync("ClosingData", requestData);
-                    }, 2000);
-                }
-
-                await _client.Clients.All.SendAsync("ReceiveNotification", "New data has been received or sent.");
-
-
-
-                return Ok(results);
-            }
-            catch (Exception ex)
-            {
-                return Conflict(ex.Message);
-            }
+            return await HandleNotification(command, "ClosingData");
         }
-
 
         [HttpGet("open-ticket")]
         public async Task<IActionResult> OpenTicketNotification([FromQuery] OpenTicketNotificationResultQuery command)
         {
-            try
-            {
-                if (User.Identity is ClaimsIdentity identity)
-                {
-                    var userRole = identity.FindFirst(ClaimTypes.Role);
-                    if (userRole != null)
-                    {
-                        command.Role = userRole.Value;
-                    }
-
-                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
-                    {
-                        command.UserId = userId;
-
-                    }
-                }
-
-                var results = await _mediator.Send(command);
-                if (results.IsFailure)
-                {
-                    return BadRequest(results);
-                }
-
-                var timerControl = _timerControl;
-                var clientsAll = _client.Clients.All;
-
-                if (timerControl != null && !timerControl.IsTimerStarted && clientsAll != null)
-                {
-                    timerControl.ScheduleTimer(async (scopeFactory) =>
-                    {
-                        using var scope = scopeFactory.CreateScope();
-                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                        var requestData = await mediator.Send(command);
-                        await clientsAll.SendAsync("OpenData", requestData);
-                    }, 2000);
-                }
-
-                await _client.Clients.All.SendAsync("ReceiveNotification", "New data has been received or sent.");
-
-                return Ok(results);
-            }
-            catch (Exception ex)
-            {
-                return Conflict(ex.Message);
-            }
+            return await HandleNotification(command, "OpenTicketData");
         }
 
         [HttpGet("ticket-comment")]
         public async Task<IActionResult> CommentNotification([FromQuery] CommentNotificationQueryResult command)
         {
-            try
-            {
-                if (User.Identity is ClaimsIdentity identity)
-                {
-
-                    if (Guid.TryParse(identity.FindFirst("id")?.Value, out var userId))
-                    {
-                        //query.Users = userId;
-                        command.UserId = userId;
-
-                    }
-                }
-
-                var results = await _mediator.Send(command);
-
-                return Ok(results);
-
-            }
-            catch(Exception ex) 
-            {
-                return Conflict(ex.Message);
-
-            }
+            return await HandleNotification(command, "CommentData");
         }
-
-
-
-
     }
 }
